8321228e5bdfcf2439ca9c83498768c34ba4d4f2
Mittenzx <103289826+Mittenzx@users.noreply.github.com>
2025-10-12 19:21:43 +0100

    Add SectorMapWidget header

---
+++ SectorMapWidget.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SectorMapWidget.generated.h"
+
+UCLASS()
+class ADASTREA_API USectorMapWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    USectorMapWidget();
+};
